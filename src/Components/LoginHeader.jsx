ee990324983ff08d1f6235c12130272ac83b3881
AS-Baba <130873763+AS-Baba@users.noreply.github.com>
2024-07-01 10:46:53 +0100

    Update LoginHeader.jsx

--- src/Components/LoginHeader.jsx
+++ src/Components/LoginHeader.jsx
@@ -5,7 +5,7 @@ export const LoginHeader = () => {
     <div className="col-md-10 container d-flex d-flex justify-content-between bg-primar mt-5 mb-3">
       <h4 className="loginHead">Login</h4>
       <p className="dontHaveAnAccount">
-        Don't have an accout?{" "}
+        Don't have an account?{" "}
         <span className="text-primary signUp">Sign Up</span>
       </p>
     </div>
